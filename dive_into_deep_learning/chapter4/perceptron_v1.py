21693e0d83b83dd9383156d8279142741fd59666
zhoukai06 <zhoukai06@zuoyebang.com>
2024-10-12 00:44:36 +0800

    add

--- dive_into_deep_learning/chapter4/perceptron_v1.py
+++ dive_into_deep_learning/chapter4/perceptron_v1.py
@@ -22,22 +22,30 @@ def relu(input):
 _trans = [torchvision.transforms.ToTensor()]
 trans = torchvision.transforms.Compose(_trans)
 train_set = torchvision.datasets.FashionMNIST("../../data", train=True, transform=trans, download=True)
+test_set = torchvision.datasets.FashionMNIST("../../data", train=True, transform=trans, download=True)
 
 batch_size = 64
 dataloader_train = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+dataloader_test = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
 
-epoch = 1
-
+epoch = 10
 cross_entropy_loss = nn.CrossEntropyLoss()
 optim = torch.optim.SGD([w1,b1,w2,b2], lr=0.01)
 for i in range(epoch):
     for X,Y in dataloader_train:
         y_hat = net(X)
         loss = cross_entropy_loss(y_hat,Y)
-        print(loss)
         loss.backward()
         optim.step()
         optim.zero_grad()
+    true_cnt = 0
+    for X,Y in dataloader_test:
+        y_hat = net(X)
+        y_hat = y_hat.argmax(dim=1)
+        y_hat = y_hat.reshape(Y.shape)
+        result = y_hat == Y
+        true_cnt += result.sum()
+    print("accurate: ",true_cnt/(len(test_set.data)))
 
 
 
